079796916bdd365efe96585fa68ec8c5b56e37f5
233zzy233 <90546380+233zzy233@users.noreply.github.com>
2021-11-08 20:27:21 +0800

    Add files via upload

---
+++ 开源/pages/mainPage/mainPage.js
@@ -0,0 +1,140 @@
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      data: [
+  
+        {
+          img: "欢迎来到牛码宝宝，好好学习天天向上！！",
+          linkurl: "",
+          linkname: "",
+          starspos: 0,
+          back_color: "gold"
+        }
+      ],
+      broadcast_arr: {
+        speed: 2.4, //滚动速度，每秒2.8个字
+        font_size: "16", //字体大小(px)
+        text_color: "#8a8a8a", //字体颜色
+    
+  
+      }
+    },
+  
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+      let ititdata = this.data.data,
+        assist = this.data.broadcast_arr,
+        this_width = 0,
+        spacing = 0,
+      speed = (this.data.broadcast_arr.speed * this.data.broadcast_arr.font_size); //m每秒行走的距离
+      
+      for (let i in ititdata) {
+        ititdata[i].starspos = wx.getSystemInfoSync().windowWidth; //以取屏幕宽度为间距
+        this_width += ititdata[i].img.length * this.data.broadcast_arr.font_size;
+        if (i != ititdata.length - 1) {
+          spacing += ititdata[i].starspos
+        }
+      }
+      let total_length = this_width + spacing;//总长
+      assist.time = total_length / speed; /**滚动时间*/
+      assist.width_mal = total_length; 
+      this.setData({
+        broadcast_arr: assist,
+        data: ititdata
+      })
+    },
+    jumpClassify(){
+      wx.switchTab({
+        url: '../classify/classify',
+        success: function (e) {
+          var page = getCurrentPages().pop();
+          if (page == undefined || page == null) return;
+          page.onLoad();
+        }
+      })
+  },
+  jumpSearch(){
+    wx.switchTab({
+      url: '../search/search',
+      success: function (e) {
+        var page = getCurrentPages().pop();
+        if (page == undefined || page == null) return;
+        page.onLoad();
+      }
+    })
+},
+jumpMine(){
+  wx.switchTab({
+    url: '../personal/personal',
+    success: function (e) {
+      var page = getCurrentPages().pop();
+      if (page == undefined || page == null) return;
+      page.onLoad();
+    }
+  })
+},
+jumpRank(){
+  wx.redirectTo({
+      url: '../artical/artical',
+    })
+},
+toArtical(){
+  wx.redirectTo({
+    url: '../artical/artical',
+  })
+},
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+  
+    },
+  
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+  
+    },
+  
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+  
+    },
+  
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+  
+    }
+  })
